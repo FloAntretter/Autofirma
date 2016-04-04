24e11cdf1710f82c78553be3fb51f195f9448748
Lukas Innerhofer <lukas.ihofer@gmail.com>
2016-04-04 03:39:39 +0200

    .cpp und .h fertig

---
+++ PKW.cpp
@@ -0,0 +1,31 @@
+#include "PKW.h"
+
+void PKW::setSitzplaetze(int _sitzplaetze)
+{
+	sitzplaetze = _sitzplaetze;
+}
+
+void PKW::setBaujahr(int _baujahr)
+{
+	baujahr = _baujahr;
+}
+
+void PKW::setStauraum(int _stauraum)
+{
+	stauraum = _stauraum;
+}
+
+int PKW::getSitzplaetze()
+{
+	return sitzplaetze;
+}
+
+int PKW::getBaujahr()
+{
+	return baujahr;
+}
+
+int PKW::getStauraum()
+{
+	return stauraum;
+}
